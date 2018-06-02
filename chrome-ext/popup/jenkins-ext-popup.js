712dc5ee11190fcd3e87fb4ea8747b6fc4bc7f91
Uri Kalish <uri.kalish@hpe.com>
2018-06-02 13:29:39 +0300

    scope separation

--- chrome-ext/popup/jenkins-ext-popup.js
+++ chrome-ext/popup/jenkins-ext-popup.js
@@ -1,35 +1,38 @@
+(function() {
 
-let localStorageHighlightCommitersKey = 'jenkins-ext-highlight-commiters';
-let highlightCommiters = '';
+	let localStorageHighlightCommitersKey = 'jenkins-ext-highlight-commiters';
+	let highlightCommiters = '';
 
-function onDomLoaded() {
-	document.getElementById('jenkins-ext-popup-cancel-button').addEventListener('click', onPopupCancel);
-	document.getElementById('jenkins-ext-popup-save-button').addEventListener('click', onPopupSave);
-	highlightCommiters = localStorage.getItem(localStorageHighlightCommitersKey) || '';
-	document.getElementById('highlight-commiters-input').value = highlightCommiters;
-}
+	function onDomLoaded() {
+		document.getElementById('jenkins-ext-popup-cancel-button').addEventListener('click', onPopupCancel);
+		document.getElementById('jenkins-ext-popup-save-button').addEventListener('click', onPopupSave);
+		highlightCommiters = localStorage.getItem(localStorageHighlightCommitersKey) || '';
+		document.getElementById('highlight-commiters-input').value = highlightCommiters;
+	}
 
-function onPopupCancel() {
-	window.close();
-}
-
-function sendHighlightCommitersToContentScript(tabs) {
-	tabs.forEach(tab => {
-		chrome.tabs.sendMessage(
-			tab.id,
-			{
-				'type': 'jenkins-chrome-ext-highlight-commiters',
-				'msg': highlightCommiters
-			}
-		);
+	function onPopupCancel() {
 		window.close();
-	});
-}
+	}
 
-function onPopupSave() {
-	highlightCommiters = document.getElementById('highlight-commiters-input').value || '';
-	localStorage.setItem(localStorageHighlightCommitersKey, highlightCommiters);
-	chrome.tabs.query({currentWindow: true,	active: true}, sendHighlightCommitersToContentScript);
-}
+	function sendHighlightCommitersToContentScript(tabs) {
+		tabs.forEach(tab => {
+			chrome.tabs.sendMessage(
+				tab.id,
+				{
+					'type': 'jenkins-chrome-ext-highlight-commiters',
+					'msg': highlightCommiters
+				}
+			);
+			window.close();
+		});
+	}
 
-document.addEventListener('DOMContentLoaded', onDomLoaded, false);
+	function onPopupSave() {
+		highlightCommiters = document.getElementById('highlight-commiters-input').value || '';
+		localStorage.setItem(localStorageHighlightCommitersKey, highlightCommiters);
+		chrome.tabs.query({currentWindow: true,	active: true}, sendHighlightCommitersToContentScript);
+	}
+
+	document.addEventListener('DOMContentLoaded', onDomLoaded, false);
+
+})();
