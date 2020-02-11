400aa61f7ccdd3406dea5f71c520c5e5049c7547
Uri Kalish <uri.kalish@microfocus.com>
2020-02-11 20:52:03 +0200

    refactor code

--- chrome-ext/content/jenkins-ext-common.js
+++ chrome-ext/content/jenkins-ext-common.js
@@ -1,4 +1,13 @@
 //let extId = 'cjmholedpdghokadoionhngnmfpeebnk';
+let buildStatusEnum = {
+	PENDING: 'PENDING',
+	RUNNING: 'RUNNING',
+	SUCCESS: 'SUCCESS',
+	WARNING: 'WARNING',
+	FAILURE: 'FAILURE',
+	ABORTED: 'ABORTED',
+	UNKNOWN: 'UNKNOWN'
+};
 let buildResult = {
 	SUCCESS:  'SUCCESS',
 	FAILURE:  'FAILURE',
@@ -34,4 +43,38 @@ function getBuildLinkElement(buildNumber) {
 		});
 	}
 	return result;
+}
+
+function getBuildStatus(buildNumber) {
+	let buildStatus = buildStatusEnum.UNKNOWN;
+	let buildLinkElm = getBuildLinkElement(buildNumber);
+	if (buildLinkElm) {
+		let parentElm = buildLinkElm.parentElement.parentElement.parentElement;
+		let statusImg = parentElm.querySelector('.build-status-link > img.icon-sm');
+		if (statusImg) {
+			if (statusImg.classList.contains('icon-blue')) {
+				buildStatus = buildStatusEnum.SUCCESS;
+			} else if (statusImg.classList.contains('icon-yellow')) {
+				buildStatus = buildStatusEnum.WARNING;
+			} else if (statusImg.classList.contains('icon-red')) {
+				buildStatus = buildStatusEnum.FAILURE;
+			} else if (statusImg.classList.contains('icon-aborted')) {
+				buildStatus = buildStatusEnum.ABORTED;
+			} else if (statusImg.classList.contains('icon-grey')) {
+				buildStatus = buildStatusEnum.PENDING;
+			} else if (statusImg.className.indexOf('-anim') !== -1) {
+				buildStatus = buildStatusEnum.RUNNING;
+			}
+		}
+	}
+	return buildStatus;
+}
+
+function addBuildPanelClass(buildNumber) {
+	let buildLinkElm = getBuildLinkElement(buildNumber);
+	if (buildLinkElm) {
+		let parentElm = buildLinkElm.parentElement.parentElement.parentElement;
+		let buildStatus = buildInfos[buildNumber].status;
+		parentElm.classList.add(`jenkins-ext-build-status--${buildStatus.toLowerCase()}`);
+	}
 }
