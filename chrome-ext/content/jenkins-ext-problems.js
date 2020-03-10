5cc159b54dbbd37bb8196fe08720150fb01dae33
Uri Kalish <uri.kalish@microfocus.com>
2020-03-10 13:58:11 +0200

    investigate build problem

--- chrome-ext/content/jenkins-ext-problems.js
+++ chrome-ext/content/jenkins-ext-problems.js
@@ -131,7 +131,7 @@ function showProblemDialog(problem) {
 	let consoleLinkElm = document.createElement('a');
 	consoleLinkElm.setAttribute('href', `/${problem.url}consoleFull`);
 	consoleLinkElm.setAttribute('target', '_blank');
-	consoleLinkElm.innerText = 'Goto console output';
+	consoleLinkElm.innerText = 'Goto Full Console Output';
 	consoleLinkElm.className = 'jenkins-ext-build-problem-dialog-console-link';
 	problemDialogElm.appendChild(consoleLinkElm);
 
