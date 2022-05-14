2c8d2862fca23270c02ef65e0051b2ca4ea85ad1
kalish <uri.kalish@gmai.com>
2022-05-14 18:28:32 +0300

    remove console.log

--- chrome-ext/content/jenkins-ext-zwsp-fixer.js
+++ chrome-ext/content/jenkins-ext-zwsp-fixer.js
@@ -1,6 +1,5 @@
 function cleanupZwsInsertedElements() {
     const zwsInsertedDomElms = document.querySelectorAll('.zws-inserted');
-    console.log(zwsInsertedDomElms);
     if (zwsInsertedDomElms && zwsInsertedDomElms.length > 0) {
         zwsInsertedDomElms.forEach(zwsInsertedDomElm => {
             zwsInsertedDomElm.innerText = zwsInsertedDomElm.innerText.replace(/[\u200B]/g, '').trim();
